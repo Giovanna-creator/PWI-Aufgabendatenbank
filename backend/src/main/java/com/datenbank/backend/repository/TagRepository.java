8ff68fa194b29f98f28393665452a4b1495dcd7f
Pharel Harold Nanseu Kombou <pharel.harold.nanseu.kombou@mni.thm.de>
2026-05-29 20:37:06 +0200

    typ Integer

--- backend/src/main/java/com/datenbank/backend/repository/TagRepository.java
+++ backend/src/main/java/com/datenbank/backend/repository/TagRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
+public interface TagRepository extends JpaRepository<Tag, Integer> {
     // Alle Tags die einen bestimmten Parent haben
     List<Tag> findByParentTag_TagId(Integer parentTagId);
 
