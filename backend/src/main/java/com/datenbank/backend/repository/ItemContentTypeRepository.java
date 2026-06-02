ab0296272311ed2c1ff7b7ac9f2e4a4f6e23fb2a
Pharel Harold Nanseu Kombou <pharel.harold.nanseu.kombou@mni.thm.de>
2026-06-02 22:00:56 +0200

    Add ItemContent repositories and controller

--- backend/src/main/java/com/datenbank/backend/repository/ItemContentTypeRepository.java
+++ backend/src/main/java/com/datenbank/backend/repository/ItemContentTypeRepository.java
@@ -1,13 +1,11 @@
 package com.datenbank.backend.repository;
 
-import com.datenbank.backend.entity.ItemContent;
+import com.datenbank.backend.entity.ItemContentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemContentRepository
-        extends JpaRepository<ItemContent, Integer> {
-
-
+public interface ItemContentTypeRepository extends JpaRepository<ItemContentType, Integer> {
 }
+
 
